b7cebc7a24b1e015699b3d8d9f4f7905c49c38f0
Ilya N. Zykin <zykin-ilya@ya.ru>
2023-07-05 15:24:59 +0300

    Rails 6. Add Routes

--- rails6-app/config/routes.rb
+++ rails6-app/config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # TheRoleManagementPanel::Routes.mixin(self)
+
+  root to: 'welcome#index'
+  resources  :users, only: [:edit, :update]
+
+  resources  :pages do
+    collection do
+      get :my
+      get :manage
+    end
+  end
 end
