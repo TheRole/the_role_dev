5d9b6d2f5ee0bcd344f81686494e13d2e5922f73
Ilya N. Zykin <zykin-ilya@ya.ru>
2023-07-08 22:33:14 +0300

    Rails 5. Some rspec setup

--- rails5-app/spec/rails_helper.rb
+++ rails5-app/spec/rails_helper.rb
@@ -7,6 +7,8 @@ require_relative '../config/environment'
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 
+Dir[Rails.root.join('../', 'the_role_specs', 'specs', 'factories', '**', '*.rb')].sort.each { |f| require f }
+
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
@@ -14,7 +16,15 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 
+# Return default text on "Access Denied" page
+def access_denied_match
+  "redirected"
+end
+
 RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
+  config.include Devise::Test::ControllerHelpers, type: :controller
+
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
