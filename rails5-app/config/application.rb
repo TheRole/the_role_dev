9952d3a3d8b3b0533e0709bef362054e093bd759
Ilya N. Zykin <zykin-ilya@ya.ru>
2023-07-08 22:35:30 +0300

    Rails 5. Some app setup

--- rails5-app/config/application.rb
+++ rails5-app/config/application.rb
@@ -11,6 +11,15 @@ module Rails5app
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.paths["app/views"] << Rails.root.join("../the_role_specs/_TEST_APP_/views")
+    config.paths["app/models"] << Rails.root.join("../the_role_specs/_TEST_APP_/models")
+    config.paths["app/controllers"] << Rails.root.join("../the_role_specs/_TEST_APP_/controllers")
+
+    config.paths["db/migrate"] << Rails.root.join("../the_role_specs/_TEST_APP_/db/migrate")
+    config.paths["config/locales"] << Rails.root.join("../the_role_specs/_TEST_APP_/locales")
+    config.paths["config/routes.rb"] << Rails.root.join("../the_role_specs/_TEST_APP_/routes.rb")
+    config.paths["config/initializers"] << Rails.root.join("../the_role_specs/_TEST_APP_/initializers")
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
